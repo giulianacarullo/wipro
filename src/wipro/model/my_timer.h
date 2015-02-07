857d6edb7aca996a75f46a5bf9a30170d5d93d75
“giulianacarullo” <giulietta.giugiu@gmail.com>
2015-02-07 18:28:08 +0100

    debugging time - timer

---
+++ src/wipro/model/my_timer.h
@@ -0,0 +1,38 @@
+/*
+ * timer.h
+ *
+ *  Created on: 02/feb/2015
+ *      Author: superjulietta
+ */
+
+#ifndef MY_TIMER_H_
+#define MY_TIMER_H_
+
+//#include "ns3/core-module.h"
+#include <iostream>
+//#include <conio>
+#include <time.h>	// class needs this inclusion
+
+using namespace std;
+class timer {
+public:
+	timer();
+	virtual ~timer();
+	void           start();
+	void           stop();
+	void           reset();
+	bool           isRunning();
+	unsigned long  getTime();
+	unsigned long getTimeInSeconds();
+	bool           isOver(unsigned long seconds);
+private:
+	bool           resetted;
+	bool           running;
+	unsigned long  beg;
+	unsigned long  end;
+};
+
+
+
+
+#endif /* TIMER_H_ */
